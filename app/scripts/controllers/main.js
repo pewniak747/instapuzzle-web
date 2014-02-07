85b7483f4d96d73330b8f65e1cbdc3f7c7ce2731
Tomasz Pewiński <pewniak747@gmail.com>
2014-02-07 21:29:21 +0100

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('instapuzzleWebApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
