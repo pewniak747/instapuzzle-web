48aa2f42f1e5eb7536ecc5e11f19949b3a9c8835
Tomasz Pewiński <pewniak747@gmail.com>
2014-03-10 18:34:01 +0100

    replace image links

--- Gruntfile.js
+++ Gruntfile.js
@@ -229,8 +229,14 @@ module.exports = function (grunt) {
     usemin: {
       html: ['<%= yeoman.dist %>/{,*/}*.html'],
       css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
+      js: ['<%= yeoman.dist %>/scripts/*.js'],
       options: {
-        assetsDirs: ['<%= yeoman.dist %>']
+        assetsDirs: ['<%= yeoman.dist %>'],
+        patterns: {
+          js: [
+            [/(images\/.*?\.(?:gif|jpeg|jpg|png|webp|svg))/gm, 'Update the JS to reference our revved images']
+          ]
+        }
       }
     },
 
